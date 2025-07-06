0666240e3528bd07fd4777e4b340e4f726f568fd
Showrov <yshowrov7@gmail.com>
2025-07-06 18:50:23 +0600

    deploy

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,9 @@ const app: Application = express();
 app.use(express.json())
 
 app.use(cors({
-    origin: "https://bookmanagementfrontend-orpin.vercel.app",
+    origin: ["https://bookmanagementfrontend-orpin.vercel.app"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true, 
  
 }))
 app.use("/api/books", bookRouter)
