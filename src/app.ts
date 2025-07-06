e9aa98886deba10270d8a1f7551efe9c103f4d49
Showrov <yshowrov7@gmail.com>
2025-07-06 18:32:22 +0600

    deploy issue

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,6 @@ const app: Application = express();
 app.use(express.json())
 
 app.use(cors({
-    origin: "https://bookmanagementfrontend-orpin.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
  
 }))
 app.use("/api/books", bookRouter)
